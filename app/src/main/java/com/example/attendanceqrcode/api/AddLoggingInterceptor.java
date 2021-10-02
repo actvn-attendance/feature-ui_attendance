bc7f3a08acf261eddc9dcfb1cd6499678bcc6029
jvm <hoangvt979@gmail.com>
2021-10-02 14:23:11 +0700

    enable usesCleartextTraffic in android 9 over

---
+++ app/src/main/java/com/example/attendanceqrcode/api/AddLoggingInterceptor.java
@@ -0,0 +1,17 @@
+package com.example.attendanceqrcode.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class AddLoggingInterceptor {
+    public static OkHttpClient setLogging(){
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
+        return okHttpClient;
+    }
+}
