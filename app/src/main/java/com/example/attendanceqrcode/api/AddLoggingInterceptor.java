a532d4bf3743ee9a04c8707845feb67eb35a8d58
jvm <hoangvt979@gmail.com>
2021-10-12 21:10:10 +0700

    edit timeout retrofit

--- app/src/main/java/com/example/attendanceqrcode/api/AddLoggingInterceptor.java
+++ app/src/main/java/com/example/attendanceqrcode/api/AddLoggingInterceptor.java
@@ -2,15 +2,20 @@ package com.example.attendanceqrcode.api;
 
 import com.example.attendanceqrcode.middleware.UnauthorizedInterceptor;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 public class AddLoggingInterceptor {
-    public static OkHttpClient setLogging(){
+    public static OkHttpClient setLogging() {
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
                 .addInterceptor(loggingInterceptor)
                 .addInterceptor(new UnauthorizedInterceptor())
                 .build();
